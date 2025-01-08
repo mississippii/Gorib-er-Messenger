931cef329118f5c342c7aec3fcf94b781e559277
Tanvir <tanvir@telcobright.com>
2025-01-08 15:25:06 +0600

    refactor code

--- Chat-Server/src/main/java/veer/chatserver/socket/WebSocketConfig.java
+++ Chat-Server/src/main/java/veer/chatserver/socket/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package veer.chatserver.websocket;
+package veer.chatserver.socket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
